cf2ff168756348b769e1b84ad1ca9c339ce25e79
miamarichka <ilchyshyn.marichka@gmail.com>
2023-06-16 20:29:34 +0300

    adding env

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require('./app');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 const { DB_HOST, PORT } = process.env;
 
 mongoose.connect(DB_HOST)
